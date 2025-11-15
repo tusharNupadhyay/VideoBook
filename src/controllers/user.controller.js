8d433cdd1be27c84156a09103c4b534215366a6d
Tushar Upadhyay <tupadhyay99@gmail.com>
2025-11-16 03:23:17 +0530

    added registeruser method in user controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,57 @@
+import { ApiError } from "../utils/ApiError.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
+import {User} from "../models/user.model.js"
+import {uploadOnCloudinary} from "../utils/cloudinary.js";
+import {ApiResponse} from "../utils/ApiResponse.js";
 
 const registerUser = asyncHandler(async (req, res) => {
-   res.status(200).json({
-    message: "TUshar upadhyay",
+  //get user details from frontend (using postman)
+  //validate (not empty)
+  //check if user already exists (through username or email)
+  //check for avatar(required) and images
+  //upload them to cloudinary
+  //create user object-create entry in db
+  //remove password and refresh token field from response
+  //check for user creation
+  //return res
+  const {fullName, username,email,password} = req.body;
+  const fields = [fullName,username,email,password];
+  if(fields.some(field => !field || field.trim()==="")){
+    throw new ApiError(400,"All fields are required");
+  }
+  //check if user already exists
+  //use await because findone returns a promise and a promise object is a truthy
+  //$or-mongodb logical operator same as 'OR'
+  const existedUser =await User.findOne({
+    $or: [{username},{email}]
   });
+  if(existedUser) throw new ApiError(409,"User with email or username already exists");
+
+  //req.body is given by express and req.files is given by multer 
+  const avatarLocalPath=req.files?.avatar[0]?.path;
+  const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+  if(!avatarLocalPath) throw new ApiError(400,"avatar file is required");
+
+  //upload them to cloudinary
+  //we will use await so it will not go to next step without uploading 
+  const avatar = await uploadOnCloudinary(avatarLocalPath);
+  const coverImage = await uploadOnCloudinary(coverImageLocalPath);
+  if(!avatar) throw new ApiError(400,"avatar file is required");
+
+ const user = await User.create({
+    fullName,
+    avatar: avatar.url,
+    coverImage: coverImage?.url || "",
+    email,
+    password,
+    username: username.toLowerCase()
+  });
+
+  const createdUser = await User.findById(user._id).select("-password -refreshToken");
+  if(!createdUser) throw new ApiError(500,"something went wrong while registering user");
+
+  return res.status(201).json( new ApiResponse(200,createdUser,"user registered successfully"))
 });
 
 export { registerUser };
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,7 +1,23 @@
 import { Router } from "express";
 import { registerUser } from "../controllers/user.controller.js";
+import { upload } from "../middlewares/multer.middleware.js";
+import {ApiError} from "../utils/ApiError.js";
 
 const router = Router();
-router.route("/register").post(registerUser);
+//since we can only handle json data in registeruser method(like email,username,etc) not files so we will use multer middleware here to handle files
+//upload(multer) middleware intercepts two fields that are sent from frontend(postman) stores them in req.files and passes the control to registeruser method
+router.route("/register").post(
+  upload.fields([
+    {
+      name: "avatar",
+      maxCount: 1,
+    },
+    {
+      name: "coverImage",
+      maxCount: 1,
+    },
+  ]),
+  registerUser
+);
 
 export default router;
