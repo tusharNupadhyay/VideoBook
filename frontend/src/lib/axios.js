2c9041564c145426a3a553067929cb6fc5bec4c4
Tushar Upadhyay <tupadhyay99@gmail.com>
2026-02-06 22:10:58 +0530

    added "axios.defaults.withCredentials = true"

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,8 @@
 import config from "./config"
 import axios from 'axios';
 
+// This ensures EVERY request globally sends cookies
+axios.defaults.withCredentials = true;
 const api = axios.create({
   baseURL: config.baseUrl,
   withCredentials: true, //When React makes requests, the browser does not send cookies by default, with this axios tells the browser to sends and receive cookies so that user won't get logged out after refresh
